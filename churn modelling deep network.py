be0520b585ca9fe1c6426c5bfa87a9e65fb938ca
Ross Baker <rossbaker16@gmail.com>
2022-02-26 14:45:30 +0000

    Small changes to a few parameters, 87%

--- churn modelling deep network.py
+++ churn modelling deep network.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 
 pd.set_option('display.max_columns', 500)
 
-df = pd.read_csv("")
+df = pd.read_csv("C:\\Users\\44748\\Desktop\\Keele\\Machine Learning\\Projects\\ANN\\annChurn\\Churn_Modelling.csv")
 
 
 X = df.iloc[:, 3:-1].values
@@ -31,6 +31,7 @@ print(len(X[0]))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 sc = StandardScaler()
+
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
@@ -40,12 +41,12 @@ X_train[0]
 model = tf.keras.Sequential([
     tf.keras.Input(shape=(12,)),
     tf.keras.layers.Dense(6, activation = 'relu'),
-    tf.keras.layers.Dense(6, activation = 'relu'),
-    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(5, activation = 'relu'),
     tf.keras.layers.Dense(1, activation = 'sigmoid')
     ])
 
-model.compile(loss = 'binary_crossentropy', optimizer ='adam', metrics = ['accuracy'])
+model.compile(loss = 'binary_crossentropy', optimizer = tf.keras.optimizers.Adam(learning_rate = 0.01), metrics = ['accuracy'])
 
-callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 train_model = model.fit(X_train, y_train, epochs = 100, validation_data =(X_test, y_test), callbacks =[callback])
